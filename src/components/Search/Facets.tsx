dfced6e9c13de1a90d79cbf654c1d9bf582ea1dc
Camden Mecklem <c@camdenmecklem.com>
2024-03-05 10:53:49 -0500

    more cleanup

--- src/components/Search/Facets.tsx
+++ src/components/Search/Facets.tsx
@@ -5,10 +5,10 @@ import fields from './fields'
 const Search = () => {
   return (
     <div>
-      {Object.values(fields).filter(f => f.facet).map((f) => (
-        <div className="p-2 bg-slate-100" key={f.value}>
-          <h2>{f.displayLabel}</h2>
-          <RefinementList attribute={f.value as string} key={f.uuid} />
+      {Object.entries(fields).filter(f => f[1].facet).map((f) => (
+        <div className="p-2 bg-slate-100" key={f[0]}>
+          <h2>{f[1].displayLabel}</h2>
+          <RefinementList attribute={f[0]} key={f[0]} />
         </div>
       ))}
     </div>
--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -1,12 +1,4 @@
 export interface Field {
-  value: string;
-  displayLabel: string;
-  show: boolean;
-  uuid?: string;
-  isUserDefined: boolean;
-}
-
-export interface HitField {
   showCaption?: boolean;
   displayLabel?: string;
   facet?: boolean;
@@ -15,8 +7,6 @@ export interface HitField {
   snippet?: number;
   render?: (hit: any) => string;
   type?: "identifier" | "showcase";
-  uuid?: string;
-  value?: string;
 }
 
 export type Locale = 'en' | 'fr'
